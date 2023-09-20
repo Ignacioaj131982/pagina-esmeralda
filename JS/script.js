e72132166e4bc7cf4685a494c2060eb45b307b24
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2023-09-19 23:56:40 -0300

    pagina con carrousel infinito de 3 fotos

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,7 @@ nav.classList.remove("visible");
 
 const grande = document.querySelector('.grande')
 const punto = document.querySelectorAll('.punto, .punto2, .punto3, .punto4, .punto5, .punto6, .punto8, .punto9, .punto10, .punto11, .punto12, .punto13, .punto14, .punto15, .punto16, .punto17, .punto18, .punto19, .punto20');
+const slideWidth = 100; // Reemplaza 100 con el ancho de tu slide en píxeles
 
 // Cuando hago CLICK en cada punto
 //saber la posicion de ese punto
@@ -41,5 +42,56 @@ punto.forEach( ( cadaPunto , i )=> {
         punto[i].classList.add('activo')
     })
 })
+
+const btnLeft = document.querySelector(".btn-left"),
+btnRight = document.querySelector(".btn-right"),
+slider = document.querySelector("#slider"),
+sliderSection = document.querySelectorAll(".slider_section");
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+moveToRight()
+}, 3000);
+
+
+let operacion = 0, 
+counter = 0,
+widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+    operacion = 0;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "none";
+    return;
+
+}  
+counter++;
+operacion = operacion + widthImg;
+slider.style.transform = `translate(-${operacion}%)`;
+slider.style.transition = "all ease .6s"   
+
+    
+}
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0) {
+    counter = sliderSection.length-1;
+    operacion = widthImg * (sliderSection.length-1)
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "none";
+return;
+} 
+operacion = operacion - widthImg;
+slider.style.transform = `translate(-${operacion}%)`
+slider.style.transition = "all ease .6s"
+
+    
+}
+
 
 
